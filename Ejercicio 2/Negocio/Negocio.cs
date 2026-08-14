774c355276e19be094e2b98618ab2fa94f85c680
Valentinac2008 <valentinac613@alu.bue.edu.ar>
2026-08-13 21:21:07 -0300

    Update Negocio.cs

--- Ejercicio 2/Negocio/Negocio.cs
+++ Ejercicio 2/Negocio/Negocio.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using solucion.datos;
+
+namespace solucion.negocio
+{
+    public class AlumnoNegocio
+    {
+        private AlumnoDatos datos = new AlumnoDatos();
+
+        public Alumno BuscarAlumno(int legajo)
+        {
+            if (legajo <= 0)
+            {
+                throw new Exception("El legajo debe ser mayor a cero.");
+            }
+
+            return datos.BuscarAlumno(legajo);
+        }
+    }
+}
