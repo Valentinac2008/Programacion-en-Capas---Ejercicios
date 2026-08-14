e010e2a849c197abdda6f9589243d19b72d20448
Valentinac2008 <valentinac613@alu.bue.edu.ar>
2026-08-13 21:22:01 -0300

    Update datos.cs

--- Ejercicio 3/Datos/datos.cs
+++ Ejercicio 3/Datos/datos.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using solucion.negocio;
+
+namespace Datos
+{
+    public class VehiculoDatos
+    {
+        private List<Vehiculo> vehiculos = new List<Vehiculo>()
+        {
+            new Vehiculo("AA123CD", "Toyota Corolla", false),
+            new Vehiculo("AB456EF", "Ford Focus", true),
+            new Vehiculo("AC789GH", "Volkswagen Golf", false)
+        };
+
+        public Vehiculo BuscarVehiculo(string patente)
+        {
+            return vehiculos.FirstOrDefault(v => v.Patente == patente);
+        }
+    }
+}
